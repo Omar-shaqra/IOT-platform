5241aad977f3e68d16e2b2ab987afe18832c60e3
Tarek <tarek22042002triki@gmail.com>
2023-10-03 15:07:17 +0100

    Update redis.js

--- models/redis/redis.js
+++ models/redis/redis.js
@@ -1,17 +1,21 @@
-const redis = require("redis");
 
-const client = redis.createClient({
+const { createClient } = require('redis');
+
+const client =  createClient({
   url: "rediss://red-ckdreasiibqc73efrvf0:4BbyTJOjnbZP1wyry2GxpITIvNFRooQI@oregon-redis.render.com:6379",
-});
+})
+client.connect();
+
 
 client.on("connect", () => console.log("redis connected"));
 
 export const addMsg = async(message) => {
   let messageKey = Math.floor(Math.random() * 10);
   const expirationTimeInSeconds = 3 * 24 * 60 * 60; // 3 days in seconds
-  client.setex(messageKey,expirationTimeInSeconds,message, () => {
-    console.log(`Message added successfully`);
-  });
+  client.setEx(messageKey,expirationTimeInSeconds,message)
+    .then(()=>{
+      console.log(`Message added successfully`);
+    });
 }
 
 const r_connect = client.connect();
