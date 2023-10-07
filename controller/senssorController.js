7f9535990b895d8c1a0b0bf90ac08066582f2450
Tarek <tarek22042002triki@gmail.com>
2023-10-07 08:06:34 +0100

    Update senssorController.js

--- controller/senssorController.js
+++ controller/senssorController.js
@@ -4,7 +4,7 @@ const  Senssor = require("../models_/senssorModule");
 const createSenssor = async (req, res) => {
     const { key, project, jop_description} = req.body;
 
-    const senssorexists = await User.findOne({ key });
+    const senssorexists = await Senssor.findOne({ key });
 
     if (senssorexists) {
         res.status(404);
@@ -23,6 +23,29 @@ const createSenssor = async (req, res) => {
         res.status(404);
         throw new Error("Senssor already exists");
     }
+}
+
+const updateSenssor = async (req, res) => {
+    const { key, project, jop_description} = req.body;
+
+    const filter = { key: key };
+
+    const update = {
+        key:key, 
+        project:project, 
+        jop_description:jop_description
+    }
+
+    try{
+        const doc = await Character.findOneAndUpdate(filter, update, { new: true });
+        res.status(200).json({ message: 'Sensor updated successfully'})
+    }catch{
+        res.status(404);
+        throw new Error("Senssor not found");
+    }
+
+
+
 }
 
 module.exports = { 
